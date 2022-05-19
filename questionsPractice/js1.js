1ec9a3fcd6ff43be0c18dee854d97aa452f13dbf
DESKTOP-6FU4FD9\lazyraven <lazyraven1703@gmail.com>
2022-05-20 00:19:44 +0530

    questionPractice.js file li append child and on id based route define of question list, id and route define in arrayList

---
+++ questionsPractice/js1.js
@@ -0,0 +1,19 @@
+console.log('reverse js file');
+
+let name = 'Nisha';
+
+function reverseElement(name) {
+  let reverseTerm = '';
+  for (let i = 1; i <= name.length; i++) {
+    if (i <= name.length) {
+      reverseTerm = reverseTerm + name[name.length - i];
+    }
+  }
+  console.log('reverseTerm called', reverseTerm);
+}
+reverseElement(name);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+console.log(false || true);
--- questionsPractice/questionPractice.js
+++ questionsPractice/questionPractice.js
@@ -1,48 +1,68 @@
-
 var arrayQuestionList = [
-    { questionNo: "Question 1:", questionPara: "Reverse element" },
-    { questionNo: "Question 2:", questionPara: "Duplicacy element remove from list/same element don't show two times" },
-    { questionNo: "Question 3:", questionPara: "Palindrome element" },
-    { questionNo: "Question 4:", questionPara: "Sum of array elements" },
-    { questionNo: "Question 5:", questionPara: "Odd/Even elements of array elements" },
-    { questionNo: "Question 6:", questionPara: "Multiply elements of array with return same length" },
-]
-
+  {
+    id: 1,
+    questionNo: 'Question 1:',
+    questionPara: 'Reverse element',
+    questionRoute: 'reverse-elejs',
+  },
+  {
+    id: 2,
+    questionNo: 'Question 2:',
+    questionPara:
+      "Duplicacy element remove from list/same element don't show two times",
+    questionRoute: 'duplicacy-elejs',
+  },
+  {
+    id: 3,
+    questionNo: 'Question 3:',
+    questionPara: 'Palindrome element',
+    questionRoute: 'palindrome-elejs',
+  },
+  {
+    id: 4,
+    questionNo: 'Question 4:',
+    questionPara: 'Sum of array elements',
+  },
+  {
+    id: 5,
+    questionNo: 'Question 5:',
+    questionPara: 'Odd/Even elements of array elements',
+  },
+  {
+    id: 6,
+    questionNo: 'Question 6:',
+    questionPara: 'Multiply elements of array with return same length',
+  },
+];
 
 // First Type
-var str1 = "";
+var str1 = '';
 arrayQuestionList.forEach((list) => {
-    str1 = `${list.questionNo} ${list.questionPara}`;
-    var ul = document.getElementById("questionList");
-    var li = document.createElement("li");
-    var itemDone = "HII HIII"
+  str1 = `${list.questionNo} ${list.questionPara}`;
+  var ul = document.getElementById('questionList');
+  var li = document.createElement('li');
 
-    console.log(li);
-    li.appendChild(document.createTextNode(str1));
-    ul.appendChild(li);
+  li.appendChild(document.createTextNode(str1));
+  ul.appendChild(li);
 
-    // li.style.cursor = pointer;
-    li.addEventListener("click", function () {
-        console.log("RENDER DATA ")
-    });
-})
-
-
-// Second Type
-// var str1 = "";
-// arrayQuestionList.forEach((list) => {
-//     str1 = `${list.questionNo} ${list.questionPara}`;
-//     // var ul = document.getElementById("questionList");
-//     var ul = document.querySelector("#questionList");
-//     // const listItems = ul.getElementsByTagName("li");
-//     const listItems = document.getElementsByTagName("li");
-
-//     const nextLiContent = document.createElement("li")
-//     nextLiContent.textContent = str1
-
-//     ul.appendChild(nextLiContent)
-//     console.log(listItems)
-// })
-
-
-
+  li.addEventListener('click', function (event) {
+    // window.location.assign(
+    //   `/questionsPractice/questionPractice.html/${list.id}`
+    // );
+    if (list.id == 1) {
+      window.location.href = 'js1.js';
+    } else if (list.id == 2) {
+      window.location.href = 'js2.js';
+    } else if (list.id == 3) {
+      window.location.href = 'js3.js';
+    } else if (list.id == 4) {
+      window.location.href = 'js4.js';
+    } else if (list.id == 5) {
+      window.location.href = 'js5.js';
+    } else if (list.id == 6) {
+      window.location.href = 'js6.js';
+    } else {
+      console.log("route didn't find");
+    }
+  });
+});
