5695f5c9e1bf4896dde763a2303f765880669098
Nisha Yadav <nishayadav1703@gmail.com>
2023-10-20 17:49:56 +0530

    fetch api 15

--- basic-tut-2023/JS/JS-Nuggets-Ytube/15-fetch.js
+++ basic-tut-2023/JS/JS-Nuggets-Ytube/15-fetch.js
@@ -6,13 +6,40 @@
 
 const url = "https://www.course-api.com/react-tours-project";
 
-fetch(url)
-  .then((res) => {
-    console.log("res", res, res.json());
+// fetch(url)
+//   .then((res) => {
+//     console.log("res", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+// 1.
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => console.log("data", data))
+//   .catch((err) => console.log(err));
+
+//2. asyn/await
+// const getTour = async () => {
+//   const res = await fetch(url);
+//   const data = res.json();
+//   console.log("data", data);
+// };
+// getTour();
+
+//2. asyn/await try-catch
+const getTour = async () => {
+  try {
+    const res = await fetch(url);
+    // const tour = res.json();
+    // console.log("tour", tour);
+    // return tour;
     return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => console.log(err));
-console.log(fetch(url));
+  } catch (error) {
+    console.log(error);
+  }
+};
+// getTour();
+console.log(getTour().then());
