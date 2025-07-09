8d4f69023540d591e849296d8e917481ac6de362
lazyraven <nishayadav1703@gmail.com>
2025-07-09 15:36:37 +0530

    deep shallow copy learning

---
+++ basic-tut-2024/Javascript/deepShallowCopy.js
@@ -0,0 +1,18 @@
+// SWhallow Copy
+const original = { name: "Nisha", skills: ["JS", "React"] };
+const shallowCopy = { ...original };
+
+shallowCopy.name = "Updated"; // Only updates the copy
+shallowCopy.skills.push("Node.js"); // Also affects the original
+
+console.log(original.skills); // ['JS', 'React', 'Node.js']
+console.log(shallowCopy.skills); // ['JS', 'React', 'Node.js']
+
+// Deep Copy
+const originalA = { name: "Nisha", skills: ["JS", "React"] };
+const deepCopy = JSON.parse(JSON.stringify(originalA));
+
+deepCopy.skills.push("Node.js");
+
+console.log(original.skills); // ['JS', 'React']
+console.log(deepCopy.skills); // ['JS', 'React', 'Node.js']
