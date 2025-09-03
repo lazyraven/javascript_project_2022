c106ffaea8737d485f41a1ed0c812ef3161786ce
lazyraven <nishayadav1703@gmail.com>
2025-09-03 12:05:22 +0530

    Added, theory for full-stack-interview quetion, functions, lexical scope, IIFE and rearrange tasks with some added css, closure.js file updated with lexical integration

--- fullstack-interview-2025-website-solution/FrontendJS/Code/closure.js
+++ fullstack-interview-2025-website-solution/FrontendJS/Code/closure.js
@@ -12,3 +12,27 @@ document.getElementById("btn").addEventListener("click", function () {
   const newCount = increment();
   document.getElementById("output").textContent = "Count: " + newCount;
 });
+
+// Lexical Scope : Here, inner() has access to x because of lexical scope.
+function outer() {
+  let x = 10;
+  function inner() {
+    console.log(x); // inner can access x from outer
+  }
+  inner();
+}
+outer();
+
+// Closure : inner() keeps access to count even after outer() has returned — that’s a closure in action
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    return count;
+  };
+}
+
+const counter = outer();
+console.log(counter()); // 1
+console.log(counter()); // 2
+console.log(counter()); // 3
