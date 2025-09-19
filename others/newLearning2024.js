3fae3267f20b2b1827977e17c226d365ba55afcc
lazyraven <nishayadav1703@gmail.com>
2025-09-19 18:36:09 +0530

    other folder move  to html css repository

--- others/newLearning2024.js
+++
@@ -1,9 +0,0 @@
-console.log("New Learning OF 2024");
-
-d3.select("body").style("text-align", "center");
-
-d3.select("h3").style("color", "blue");
-
-let gfg = d3.greatest([5, 4, 3, 2, 1]);
-let resultDiv = document.getElementById("result");
-resultDiv.innerText = "The greatest number is: " + gfg;
