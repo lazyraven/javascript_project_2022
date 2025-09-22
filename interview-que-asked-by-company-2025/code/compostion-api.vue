8dec579e06339785d48c259a53a51d0e0035b645
lazyraven <nishayadav1703@gmail.com>
2025-09-22 14:45:12 +0530

    option api and composition api learning in VueJS

---
+++ interview-que-asked-by-company-2025/code/compostion-api.vue
@@ -0,0 +1,24 @@
+<!-- 2. Example Code: Counter Component -->
+<template>
+    <div>
+        <p>Count : {{ count }}</p>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// reactive state
+const count = ref(0)
+
+// method
+const increment = () => {
+    count.value++
+}
+
+</script>
+
+<!-- We use ref(0) to make count reactive.
+Access value with .value.
+The setup() (or <script setup>) is the entry point for Composition API. -->
