db399953591bf6f30bd980174d7c319cb4622210
lazyraven <nishayadav1703@gmail.com>
2025-09-23 10:29:59 +0530

    v-if and v-show learning part in vue js

---
+++ interview-que-asked-by-company-2025/code/v-if&v-show.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <!-- v-if: element is created/destroyed -->
+        <p v-if="isLoggedIn">Welcome back!</p>
+        <p v-else>Please log in</p>
+
+        <!-- v-show: element always in DOM, just hidden -->
+        <p v-show="showTips">Here are some helpful tips...</p>
+
+        <button @click="toggle">Toggle</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isLoggedIn: false,
+            showTips: true
+        }
+    },
+    methods: {
+        toggle() {
+            this.isLoggedIn = !this.isLoggedIn;
+            this.showTips = !this.showTips;
+        }
+    }
+}
+</script>
