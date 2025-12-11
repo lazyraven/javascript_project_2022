f9946b8df73a891cd6f398d799649fdcd821bd14
lazyraven <nishayadav1703@gmail.com>
2025-12-11 21:55:05 +0530

    slice practice

---
+++ basic-tut-2025/Topicwise/String/slice.js
@@ -0,0 +1,16 @@
+// Task 1: Get the last 3 characters of a string.
+// Input: "developer"
+// Output: "per"
+
+const str = "developer";
+console.log(str.slice(-3));
+
+// 1. Basic Examples (Strings & Arrays)
+// 1.1 Slice part of a string
+const text = "JavaScript";
+const part = text.slice(0, 4);
+console.log("Part Result > ", part);
+
+// 1.2 Slice last characters
+const str1 = "developer";
+console.log("Resule > ", str1.slice(-3));
