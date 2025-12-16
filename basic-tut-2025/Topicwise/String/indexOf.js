81f9a37c77ab76131c4a5266010b9a0d2d28fa3d
lazyraven <nishayadav1703@gmail.com>
2025-12-16 20:31:54 +0530

    added indexof quesons

---
+++ basic-tut-2025/Topicwise/String/indexOf.js
@@ -0,0 +1,16 @@
+// ✔ Returns the starting index
+// ❌ Returns -1 if not found
+
+// 1️⃣ Easy Level — Basics
+// Example 1: Find position in a string
+const str = "JavaScript is awesome"; // 4
+const result = str.indexOf("Script");
+console.log("result", result);
+
+const word = "hello"; // 1
+const wordResult = word.indexOf("e");
+console.log("wordResult", wordResult);
+
+const arr = ["apple", "banana", "mango"]; // 1
+const arrResult = arr.indexOf("banana");
+console.log("arrResult", arrResult);
