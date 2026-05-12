0fe39c210854aa5c5cb16094e95856e535995f87
Nisha Yadav <nishayadav1703@gmail.com>
2026-05-12 11:40:02 +0530

    Find unique elements and duplicate question practice, added dsa roadmap sequence

--- basic-tut-2026/Practice/08.js
+++ basic-tut-2026/Practice/08.js
@@ -15,25 +15,43 @@ const input1 = [1, 2, 3, 2, 4, 1]
 const input2 = [5, 6, 7, 8]
 const input3 = [1, 1, 1, 2, 2, 3]
 
-function find_duplicate(arr) {
-    const uniqueValues = []
-    const result = []
-    for (num of arr) {
-        if (!uniqueValues.includes(num)) {
-            uniqueValues.push(num);
-        } else if(!result.includes(num)) { // this condition take it for input3 problem, because if I directly use else it'll give output is [1,1,2], so need to remove duplicate also from here
-            result.push(num);
-        }
-    }
-    console.log("uniqueValues", uniqueValues);
-    return result;
-}
-console.log("find_duplicate result1", find_duplicate(input1));
-console.log("find_duplicate result2", find_duplicate(input2));
-console.log("find_duplicate result3", find_duplicate(input3));
+// function find_duplicate(arr) {
+//     const uniqueValues = []
+//     const result = []
+//     for (num of arr) {
+//         if (!uniqueValues.includes(num)) {
+//             uniqueValues.push(num);
+//         } else if(!result.includes(num)) { // this condition take it for input3 problem, because if I directly use else it'll give output is [1,1,2], so need to remove duplicate also from here
+//             result.push(num);
+//         }
+//     }
+//     console.log("uniqueValues", uniqueValues);
+//     return result;
+// }
+// console.log("find_duplicate result1", find_duplicate(input1));
+// console.log("find_duplicate result2", find_duplicate(input2));
+// console.log("find_duplicate result3", find_duplicate(input3));
 
 // Time Complexity
 // Your current approach uses includes() multiple times.
 // Complexity:
 // O(n²)
 
+
+function find_duplicate(arr) {
+    const uniqueValues = new Set()
+    const result = new Set()
+    for (num of arr) {
+        if (!uniqueValues.has(num)) {
+            uniqueValues.add(num)
+        } else{
+            result.add(num);
+        }
+    }
+    console.log("uniqueValues", uniqueValues);
+    return result;
+    // return uniqueValues;
+}
+console.log("find_duplicate result1", find_duplicate([1, 2, 3, 2, 4, 1]));
+console.log("find_duplicate result2", find_duplicate([5, 6, 7, 8]));
+console.log("find_duplicate result3", find_duplicate([1, 1, 1, 2, 2, 3]));
