5f0a2a7170102f37d3f32902741e9ba8732eaa36
Nisha Yadav <nishayadav1703@gmail.com>
2026-08-01 20:18:59 +0530

    solution added for typescript

--- fullstack-interview-2025-website-solution/FrontendReact/Code/03_props.jsx
+++ fullstack-interview-2025-website-solution/FrontendReact/Code/03_props.jsx
@@ -10,3 +10,34 @@ function App() {
     </>
   );
 }
+
+
+import { useState } from "react";
+
+// Ex:2
+function Parent() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Display count={count} />
+      <Controls onIncrement={() => setCount(c => c + 1)} />
+    </>
+  );
+}
+
+function Display({ count }) {
+  return <h2>Count: {count}</h2>;
+}
+
+function Controls({ onIncrement }) {
+  return (
+    <div>
+      <button onClick={onIncrement}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default Parent;
